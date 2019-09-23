075d5711dbb80b686bd4e48f8bfdc071759e7699
Laura Berge <lauraeberge@gmail.com>
2019-09-23 19:16:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -25,7 +25,7 @@ class Application
       end
       
     elsif req.path.match(/add/)
-      search_term = req.params[item:]
+      search_term = req.params[:item]
       
       if @@items.include?(search_term)
         resp.write "added #{search_term}"

5f59fd25d7c188ed8884b81e5702e44ebc19cec0
Laura Berge <lauraeberge@gmail.com>
2019-09-23 19:07:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -21,9 +21,9 @@ class Application
     elsif req.path.match(/add/)
       search_term = req.params["q"]
       if @@items.include?(search_term)
-        
+        resp.write "added #{search_term}"
       else
-        
+        resp.write "We don't have that item"
       end
     else
       resp.write "Path Not Found"

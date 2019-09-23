228aaf6ed032b74982b7658fa43c076d4bd18537
Laura Berge <lauraeberge@gmail.com>
2019-09-23 19:11:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+      
       if @@cart.empty?
         resp.write "Your cart is empty"
       else
@@ -22,13 +23,16 @@ class Application
           resp.write "#{item}\n"
         end
       end
+      
     elsif req.path.match(/add/)
       search_term = req.params["q"]
+      
       if @@items.include?(search_term)
         resp.write "added #{search_term}"
       else
         resp.write "We don't have that item"
       end
+      
     else
       resp.write "Path Not Found"
     end
